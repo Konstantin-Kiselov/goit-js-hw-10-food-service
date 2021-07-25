10988e5591c48a06a60a7b1fa31c83c7754945b6
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-07-26 00:10:17 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 
 import foodCardsTemplates from './templates/cards.hbs';
-console.log(foodCardsTemplates);
+// console.log(foodCardsTemplates);
 
 import menu from './menu.json';
 console.log(menu);
@@ -17,7 +17,7 @@ const menuListRef = document.querySelector('.js-menu');
 //console.log(menuListRef);
 
 const cardsMenuMarkup = createMenuList(menu);
-//console.log(cardsMenuMarkup);
+// console.log(cardsMenuMarkup);
 
 menuListRef.insertAdjacentHTML('afterbegin', cardsMenuMarkup);
 
@@ -32,7 +32,7 @@ bodyRef.classList.add(Theme.LIGHT);
 
 // ======= Добавляем функционал изменения темы при нажатии на чекбокс =======
 const selectCheckbox = document.querySelector('#theme-switch-toggle');
-console.log(selectCheckbox);
+// console.log(selectCheckbox);
 
 // ======= При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme =======
 selectCheckbox.addEventListener('change', onSelectThemes);
@@ -41,29 +41,18 @@ selectCheckbox.addEventListener('change', onSelectThemes);
 function onSelectThemes(event) {
     if (selectCheckbox.checked) {
         bodyRef.classList.remove(Theme.LIGHT);
-        bodyRef.classList.add(Theme.DARK);
-
-        const darkTheme = bodyRef.getAttribute('class');
-        console.log(darkTheme);
-        
-        localStorage.theme = darkTheme;
-        // console.log(localStorage.theme);
+        bodyRef.classList.add(Theme.DARK);   
     }
     else {
         bodyRef.classList.remove(Theme.DARK);
         bodyRef.classList.add(Theme.LIGHT);
-
-        const lightTheme = bodyRef.getAttribute('class');
-        console.log(lightTheme);
-
-        localStorage.theme = lightTheme;
     }
+
+    localStorage.theme = bodyRef.getAttribute('class');
+    console.log(localStorage.theme);
 }
 
 // ======= Если при загрузке страницы тема тёмная, ставим свойство checked в true, чтобы ползунок сдвинулся в правильное положение. =======
-const switchMarkerRef = document.querySelector('.theme-switch__marker');
-console.log(switchMarkerRef);
-
 if (localStorage.theme === Theme.DARK) {
     selectCheckbox.checked = true;
 
